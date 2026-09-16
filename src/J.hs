3c204bc8e6e14230b7d7c13f5380ee63adc8f499
Diogo Gomes de Araújo <diogoaraujo2004@gmail.com>
2026-09-16 14:51:32 +0100

    progress on infer

--- src/J.hs
+++ src/J.hs
@@ -1,7 +1,86 @@
 module J where
 
+import Ast
 import Typed
+import Data.Map as M
+import Data.List as L
+import Data.Set as S
 
-type NCtr = Int
+type Ctr = Int
 
-type AlgorithmJ = (NCtr, Env)
+type J = (Ctr, Aliases)
+
+data UnifyErr = RecursiveType | Impossible
+
+type InferResult a = Either UnifyErr a
+
+infer' :: J -> Term -> Env -> InferResult (Mono, J)
+infer' j (Var v) env
+    = case L.lookup v env of
+           Just (alphas, t) ->
+                let (new_vars, j') = newVars j
+                               $ L.length alphas in
+                return $ (inst new_vars (alphas, t), j')
+infer' j (e1 :@ e2) env
+    = do (t1, j1) <- infer' j e1 env
+         (t2, j2) <- infer' j1 e2 env
+         (t', j') <- pure $ newVar j2
+         j''      <- unify j' (t1, arrow t2 (TVar t'))
+         pure $ (TVar t', j'')
+infer' j (Lambda xs e) env
+    = let (ts, j') = newVars j $ L.length xs
+          env'      = L.foldl (\acc (x, t) ->
+                                bind acc x $ mono (TVar t))
+                        env (zip xs (S.toList ts)) in
+      do (t', j'') <- infer' j' e env'
+         tvs      <-
+            let ts' = S.toList ts in
+            pure $ L.foldl (\acc t -> arrow acc (TVar t))
+                           (TVar $ L.head ts')
+                           (L.tail ts')
+         pure $ (arrow tvs t', j'')
+
+
+-- algorithm J
+
+newJ :: J
+newJ = (0, M.empty)
+
+-- unify
+
+unify :: J -> (Mono, Mono) -> InferResult J
+unify (ctr, aliases) (t1, t2)
+    = let ct1 = canonicalize aliases t1
+          ct2 = canonicalize aliases t2 in
+      case (ct1, ct2) of
+           (ct1', ct2') | ct1' == ct2' -> Right (ctr, aliases)
+           (TApp f1 ts1, TApp f2 ts2)
+                | f1 == f2
+                  && length ts1 == length ts2 ->
+                      let iter = L.zip ts1 ts2 in
+                      L.foldl (\acc (t1', t2') ->
+                          case acc of
+                            Left e -> Left e
+                            _      -> unify (ctr, aliases)
+                                            (t1', t2')) (Right (ctr, aliases))
+                                            iter
+           (TVar v, t) | occurs t v -> Left RecursiveType
+                       | otherwise  -> Right (ctr, M.insert v t aliases)
+           _ -> Left Impossible
+
+
+
+-- new variables
+
+newVar :: J -> (Identity, J)
+newVar (ctr, aliases)
+    = ("_" ++ show ctr,
+        (ctr + 1, aliases))
+
+newVars :: J -> Int -> (Set Identity, J)
+newVars j count
+    = let (l, j') = L.foldl (\(acc, j') _ ->
+            let (new_v, new_j) = newVar j' in
+            (acc ++ [new_v], new_j)) ([], j)
+            $ L.replicate count () in
+      (S.fromList l, j')
--- src/Typed.hs
+++ src/Typed.hs
@@ -1,8 +1,9 @@
 module Typed where
 
 import Ast
-import Data.Set
-import Data.Map
+import Data.Set as S
+import Data.List as L
+import Data.Map as M
 
 type AFunc = Identity
 type AVar  = Identity
@@ -13,4 +14,105 @@ data Mono = TVar Identity
 
 type Poly = (Set AVar, Mono)
 
-type Env = Map AVar Mono
+type Aliases = Map AVar Mono
+
+type Binding = (Identity, Poly)
+
+type Env = [Binding]
+
+-- generalize and instantiate
+
+gen :: Env -> Mono -> Poly
+gen env t
+    = let alpha = fvMonoEnv t env in
+      (alpha, t)
+
+inst :: Set Identity -> Poly -> Mono
+inst new_vars (alphas, t)
+    = let vars = L.zip
+            (S.toList new_vars)
+            (S.toList alphas) in
+      L.foldl (\acc (alpha, beta) ->
+          replace alpha (TVar beta) acc) t vars
+
+
+-- free variables
+
+fvMono :: Mono -> Set Identity
+fvMono (TVar v)
+    = S.singleton v
+fvMono (TApp _ ts)
+    = L.foldl (\acc t ->
+        S.union (fvMono t) acc) S.empty ts
+
+fvPoly :: Poly -> Set Identity
+fvPoly (vs, t) =
+    fvMono t S.\\ vs
+
+fvEnv :: Env -> Set Identity
+fvEnv env
+    = L.foldl (\acc (_, t) ->
+        fvPoly t `S.union` acc )
+            S.empty env
+
+fvMonoEnv :: Mono -> Env -> Set Identity
+fvMonoEnv t env
+    = fvMono t S.\\ fvEnv env
+
+-- replace
+
+replace :: Identity -> Mono -> Mono -> Mono
+replace a t (TVar v)
+    | v == a    = t
+    | otherwise = TVar v
+replace a t (TApp f ts)
+    = TApp f $ L.map (replace a t) ts
+
+-- canonicalize
+
+canonicalize :: Map AVar Mono -> Mono -> Mono
+canonicalize aliases (TVar a)
+    = case M.lookup a aliases of
+           Just t  -> canonicalize aliases t
+           Nothing -> TVar a
+canonicalize aliases (TApp f ts)
+    = TApp f $ L.map
+        (canonicalize aliases) ts
+
+-- occurs
+occurs :: Mono -> AVar -> Bool
+occurs (TVar v) v' = v == v'
+occurs (TApp f vs) v'
+    = f == v' || L.foldl
+                    (\acc v -> acc || occurs v v')
+                    False vs
+
+-- convert declared types
+
+fromDeclaredType :: DeclaredType -> Mono
+fromDeclaredType (DVar v)
+    = TVar v
+fromDeclaredType (DList t)
+    = TApp "list" [fromDeclaredType t]
+fromDeclaredType (t1 ::-> t2)
+    = TApp "->" [ fromDeclaredType t1
+                , fromDeclaredType t2 ]
+
+-- arrow
+
+arrow :: Mono -> Mono -> Mono
+arrow t1 t2
+    = TApp "->" [t1, t2]
+
+mono :: Mono -> Poly
+mono t = (S.empty, t)
+
+-- environment
+
+bind :: Env -> Identity -> Poly -> Env
+bind env v t
+    = env ++ [(v, t)]
+
+getInEnv :: Env -> Identity -> Maybe Poly
+getInEnv env v
+    = L.lookup v env
